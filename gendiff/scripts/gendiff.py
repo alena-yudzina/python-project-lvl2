f9a4a85cdd484cd1ebb28379672bab26296b6dd9
alena-yudzina <lenok.ok.lol@gmail.com>
2021-02-18 18:43:02 +0530

    added -f flag to parameters

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,7 @@ def main():
 
 
 parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('-f', '--format', help='set format of output')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
 args = parser.parse_args()
